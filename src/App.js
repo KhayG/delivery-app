a0a672f2f7b172a6450c2102c1fabca478117e93
Abdulhay <116716959+KhayG@users.noreply.github.com>
2023-03-25 19:48:19 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Header } from './components';
+
+const App = () => {
+  return (
+    <div className="w-screen h-auto flex flex-col bg-primary">
+        <Header />
+
+        <main className="mt-24 p-8 w-full">
+            <Routes>
+                <Route path="/*" element={<MainContainer />} />
+                <Route path="/createItem" element={<CreateContainer />} />
+            </Routes>
+        </main>
+    </div>
+  );
+};
+
+export default App;
