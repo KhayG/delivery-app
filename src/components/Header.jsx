a00e046a32f9324aab2bb605be2e8cafd161f850
Abdulhay <116716959+KhayG@users.noreply.github.com>
2023-03-27 00:35:56 +0500

    git init

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { MdShoppingBasket } from "react-icons/md";
+import React, { useState } from 'react';
+import { MdShoppingBasket, MdAdd, MdLogout } from "react-icons/md";
 import { motion } from 'framer-motion';
 
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
@@ -17,15 +17,21 @@ const Header = () => {
 
     const [{user}, dispatch] = useStateValue()
 
+    const [isMenu, setIsMenu] = useState(false)
+
     const login = async () => {
-        const {
-            user: { refreshToken, providerData },
-        } = await signInWithPopup(firebaseAuth, provider);
-        dispatch({
-            type: actionType.SET_USER,
-            user: providerData[0],
-        });
-        localStorage.setItem('user', JSON.stringify(providerData[0]));
+        if (!user) {
+            const {
+                user: { refreshToken, providerData },
+            } = await signInWithPopup(firebaseAuth, provider);
+            dispatch({
+                type: actionType.SET_USER,
+                user: providerData[0],
+            });
+            localStorage.setItem('user', JSON.stringify(providerData[0]));
+        } else {
+            setIsMenu(!isMenu);
+        }
     };
 
   return ( 
@@ -68,6 +74,23 @@ const Header = () => {
                         className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
                         onClick={login}
                     />
+                    {
+                        isMenu && (
+                            <motion.div 
+                                initial={{opacity: 0, scale: 0.6}}
+                                animate={{opacity: 1, scale: 1}}
+                                exit={{opacity: 0, scale: 0.6}} 
+                                className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0"
+                            >
+                                { user && user.email === "jacksonvineamnotlos@gmail.com" && (
+                                    <Link to={"/createItem"}>
+                                        <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-200 transition-all duration-100 ease-in-out text-textColor text-base">New Item <MdAdd /></p>
+                                    </Link>
+                                )}
+                                <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-200 transition-all duration-100 ease-in-out text-textColor text-base">Logout <MdLogout /></p>
+                            </motion.div>
+                        )
+                    }
                 </div>
             </div>
         </div>
