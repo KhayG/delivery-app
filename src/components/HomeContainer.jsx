a8508814e9bbdcbfe3132b50e1ba8d1bae0f2e88
Abdulhay <116716959+KhayG@users.noreply.github.com>
2023-06-19 20:03:42 +0500

    git init

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Delivery from '../img/delivery.png';
 import HeroBg from '../img/heroBg.png';
-import I1 from "../img/i1.png";
+import { heroData } from '../utils/data';
 
 const HomeContainer = () => {
   return (
@@ -36,16 +36,18 @@ const HomeContainer = () => {
           alt="hero-bg" 
           />
           
-          <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center px-32 py-4">
-            <div className="w-190 p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center">
-              <img src={I1} className="w-40 -mt-20" alt="I1" />
-              <p className="text-xl font-semibold text-textColor mt-4">Icecream</p>
+          <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center px-32 py-4 gap-4 flex-wrap">
+            {heroData && heroData.map(n =>(
+              <div key={n.id} className="lg:w-190 p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center drop-shadow-lg">
+              <img src={n.imageSrc} className="w-20 lg:w-40 -mt-10 lg:-mt-20" alt="I1" />
+              <p className="text-base lg:text-xl font-semibold text-textColor mt-2 lg:mt-4">{n.name}</p>
             
-              <p className="text-sm text-lighttextGray font-semibold my-3">Chocolate & vanilla</p>
+              <p className="text-[12px] lg:text-sm text-lighttextGray font-semibold my-1 lg:my-3">{n.decp}</p>
 
-              <p className="text-sm font-semibold text-headingColor"><span className="text-xs text-red-600">$</span> 5.25</p>
+              <p className="text-sm font-semibold text-headingColor"><span className="text-xs text-red-600">$</span>{n.price}</p>
 
             </div>
+            ))}
           </div>
       </div>
     </section>
--- src/utils/data.js
+++ src/utils/data.js
@@ -1,5 +1,35 @@
 import I1 from "../img/i1.png";
+import F1 from "../img/f1.png";
+import C3 from "../img/c3.png";
+import FI1 from "../img/fi1.png";
 
-const heroData = [
-    {id : 1, name : 'Icecream', decp : 'Chocolate & Vanilla', price : '5.25', imageSrc : I1},
+export const heroData = [
+    {
+        id: 1,
+        name: 'Icecream',
+        decp: 'Chocolate & Vanilla',
+        price: '5.25',
+        imageSrc: I1
+    },
+    {
+        id: 2,
+        name: 'Strawberries',
+        decp: 'Fresh Strawberries',
+        price: '10.25',
+        imageSrc: F1
+    },
+    {
+        id: 3,
+        name: 'Chicken Kebab', 
+        decp: 'Mixed Kebab Plate', 
+        price: '8.25', 
+        imageSrc: C3
+    },
+    {
+        id: 4,
+        name: 'Fish Kebab', 
+        decp: 'Mixed Fish Kebab', 
+        price: '16.25',
+        imageSrc: FI1
+    },
 ]
