dd25c79774137df8e025ade160ac1c682dc3c91d
Abdulhay <116716959+KhayG@users.noreply.github.com>
2023-06-19 20:10:24 +0500

    git init

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -36,7 +36,7 @@ const HomeContainer = () => {
           alt="hero-bg" 
           />
           
-          <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center px-32 py-4 gap-4 flex-wrap">
+          <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center lg:px-32 py-4 gap-4 flex-wrap">
             {heroData && heroData.map(n =>(
               <div key={n.id} className="lg:w-190 p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center drop-shadow-lg">
               <img src={n.imageSrc} className="w-20 lg:w-40 -mt-10 lg:-mt-20" alt="I1" />
